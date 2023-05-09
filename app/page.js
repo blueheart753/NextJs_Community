18764fbd1dac9f1ff45f3c882a5312d4c4e7496e
kimminam <bluemind8520@gmail.com>
2023-05-09 18:51:32 +0900

    z

--- app/page.js
+++ app/page.js
@@ -1,14 +1,21 @@
-import { connectDB } from '@/util/database';
-import { MongoClient } from 'mongodb';
+import { connectDB } from "@/util/database";
+import { MongoClient } from "mongodb";
 
 export default async function Home() {
   let client = await connectDB;
-  const db = client.db('forum');
-  let result = await db.collection('post').find().toArray();
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
 
   return (
-    <div>
-      <div></div>
+    <div className="container">
+      <div>
+        <div className="Box">
+          <a href="http://localhost:3000/list" className="moveList">
+            리스트로 이동
+          </a>
+          <button className="darkMode">다크모드</button>
+        </div>
+      </div>
     </div>
   );
 }
